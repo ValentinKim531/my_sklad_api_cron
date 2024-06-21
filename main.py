91a2ff389dc5995fee4a68a66b16ec7dcc2ef7e1
Valentin Kim <valentink@yandex.ru>
2024-06-21 14:24:53 +0500

    logging added

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 
-import logging
-
-# Настройка логирования
-logging.basicConfig(level=logging.INFO, filename='/app/cron.log', filemode='a', format='%(asctime)s - %(message)s')
-
-
-logging.info("main.py is run")
 
 
 if __name__ == "__main__":
-    logging.info("Script started")
+    print(123)
 
